5ecf9502fefc813d54561cfa4f39a72b8c66e4a7
Luka <lukafurlan.dev@gmail.com>
2023-07-23 15:39:50 +0200

    Moved E2E out of registry src

--- apps/registry/e2e/did.spec.ts
+++ apps/registry/e2e/did.spec.ts
@@ -1,6 +1,6 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { AppModule } from '../app.module';
+import { AppModule } from '../src/app.module';
 import request from 'supertest';
 import {
   DidDocument,
