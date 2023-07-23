1bccd67319fe93a8b1fbc609aba3cce7d7d72b61
Luka <lukafurlan.dev@gmail.com>
2023-07-23 11:36:28 +0200

    Added comments to tests

--- apps/registry/src/e2e/did.spec.ts
+++ apps/registry/src/e2e/did.spec.ts
@@ -31,6 +31,11 @@ describe('Did', () => {
   });
 
   it('should register did', async () => {
+    /**
+     * Get registries public DID document. This will be used
+     * to get the public encryption key of registry, with which
+     * the create did payload will be encrypted.
+     */
     const getRegistryDidDocumentResponse = await request(
       application.getHttpServer()
     )
@@ -41,16 +46,33 @@ describe('Did', () => {
       'did:web:identity.decentrl.network'
     );
 
+    /**
+     * Load registry DID document to validate it.
+     */
     const registryDidDocument = new DidDocumentBuilder()
       .load(getRegistryDidDocumentResponse.body)
       .build();
 
+    /**
+     * Generate DID, encryption and signing key pairs.
+     * The domain is set to registry domain, so that the did
+     * generated will be resolvable to the specific registry.
+     *
+     * Node P256 key pair generators are used to generate the keys.
+     * If you want to use browser P256 key pair generators, you can
+     * import them from @decentrl/utils/browser.
+     */
     const did = await generateDid(
       'identity.decentrl.network',
       generateP256ECDHKeyPair,
       generateP256KeyPair
     );
 
+    /**
+     * Generate DID document. DID document is public and only
+     * contains DID public keys. Public keys can be used by other entities
+     * in the network to send you encrypted payloads.
+     */
     const didDocument = generateDidDocument(did, {
       alias: 'MrRobot',
     });
@@ -59,18 +81,29 @@ describe('Did', () => {
       did.keys.signingKeyPair.public.kid as string
     }`;
 
+    /**
+     * Create a signature of DID document. This signature will be used
+     * to verify that the DID document was created by the DID owner.
+     */
     const didDocumentSignature = await signPayload(
       did.keys.signingKeyPair.private,
       publicSigningKid,
       JSON.stringify(didDocument)
     );
 
+    /**
+     * Get registry routing key from its DID document. Routing key is used
+     * to encrypt the payload, so that only the registry can decrypt it.
+     */
     const registryRoutingKey = getRegistryRoutingKey(registryDidDocument);
 
     if (!registryRoutingKey) {
       throw new Error('Registry routing key not found');
     }
 
+    /**
+     * Encrypt the DID document signature using registries routing key.
+     */
     const encryptedDidDocumentPayload = await encryptPayload(
       didDocumentSignature,
       did.keys.encryptionKeyPair.private,
@@ -78,6 +111,15 @@ describe('Did', () => {
       registryRoutingKey.id
     );
 
+    /**
+     * Post encrypted payload to the registry.
+     *
+     * On this step, the registry will first decrypt the payload using its
+     * private key. Then it will verify the signature of the DID document.
+     *
+     * If the signature is valid, the registry will store the DID document
+     * in its database.
+     */
     await request(application.getHttpServer())
       .post('/')
       .send({
@@ -87,6 +129,9 @@ describe('Did', () => {
 
     const id = didDocument.id.split(':')[3];
 
+    /**
+     * The identities DID document should now be publicly available
+     */
     const getDidDocumentResponse = await request(application.getHttpServer())
       .get(`/${id}/did.json`)
       .expect(200);
@@ -98,6 +143,10 @@ describe('Did', () => {
       alias: 'Elliot',
     }
 
+    /**
+     * Updating the DID document is basically the same process, just with
+     * different HTTP method (put).
+     */
     const updatedDidDocumentSignature = await signPayload(
       did.keys.signingKeyPair.private,
       publicSigningKid,
